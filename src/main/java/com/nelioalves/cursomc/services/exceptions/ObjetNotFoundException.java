7b531ac388fe05014629abf18ecd32c2442eec2c
AndreMBarreira <andremilla.am@gmail.com>
2022-06-30 17:00:57 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/nelioalves/cursomc/services/exceptions/ObjetNotFoundException.java
@@ -0,0 +1,15 @@
+package com.nelioalves.cursomc.services.exceptions;
+
+public class ObjetNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjetNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjetNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
